ea01d30ec77c7cfca4f658db3184e6c315622990
omric <37829365+ohcu@users.noreply.github.com>
2018-11-28 20:35:44 -0500

    Updated generator
    Improved readability

--- DiscountGenerator/src/DiscountGenerator.java
+++ DiscountGenerator/src/DiscountGenerator.java
@@ -9,58 +9,26 @@ public class DiscountGenerator {
 
 	static Random r = new Random();
 
-	public DiscountGenerator(){
-
-	}
-
-	public static void main(String[] args){
-		DiscountGenerator g = new DiscountGenerator();
-		
-		int n5 = 0;
-		int n10 = 0;
-		int n15 = 0;
-		int n20 = 0;
-		
-		for(int i = 0; i < 60; i++){
-			int d = g.getDiscount();
-			System.out.println(i + ": " + d);
-			if(d == 5) n5++;
-			if(d == 10) n10++;
-			if(d == 15) n15++;
-			if(d == 20) n20++;
-			
-			if(d == -1) break;
-		}
-		
-		System.out.println("Number of $5 discounts: " + n5);
-		System.out.println("Number of $10 discounts: " + n10);
-		System.out.println("Number of $15 discounts: " + n15);
-		System.out.println("Number of $20 discounts: " + n20);
-
-	}
-
 	// Returns a valid discount
 	public int getDiscount(){
-		int discount = 0;
-
 		while(true){	// keep generating a discount until there is a discount that is available
 			if(num5 == 0 && num10 == 0 && num15 == 0 && num20 == 0) return -1;
-			discount = generateNum();
+			int discount = generateNum();
 			if(discount == 20 && num20 > 0) {
 				num20--;
-				return discount;
+				return 20;
 			}
 			else if(discount == 15 && num15 > 0) {
 				num15--;
-				return discount;
+				return 15;
 			}
 			else if(discount == 10 && num10 > 0) {
 				num10--;
-				return discount;
+				return 10;
 			}
 			else if(discount == 5 && num5 > 0) {
 				num5--;
-				return discount;
+				return 5;
 			}
 		}
 	}
@@ -78,7 +46,31 @@ public class DiscountGenerator {
 		else if(random < 50) return 20;
 		return 0;
 	}
-
-
+	
+	
+	// Testing method
+		public static void main(String[] args){
+			DiscountGenerator g = new DiscountGenerator();
+			
+			int n5 = 0;
+			int n10 = 0;
+			int n15 = 0;
+			int n20 = 0;
+			
+			for(int i = 0; i < 60; i++){
+				int d = g.getDiscount();
+				System.out.println(i + ": " + d);
+				if(d == 5) n5++;
+				if(d == 10) n10++;
+				if(d == 15) n15++;
+				if(d == 20) n20++;
+				if(d == -1) break;
+			}
+			
+			System.out.println("Number of $5 discounts: " + n5);
+			System.out.println("Number of $10 discounts: " + n10);
+			System.out.println("Number of $15 discounts: " + n15);
+			System.out.println("Number of $20 discounts: " + n20);
+		}
 
 }
